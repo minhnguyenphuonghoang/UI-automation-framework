71f660018928c8d1959017df1a18c241dadecd77
Minh Nguyen <minhnguyenphuonghoang@gmail.com>
2017-01-19 02:18:48 +0700

    add test cases

---
+++ src/testCases/test.java
@@ -0,0 +1,14 @@
+package testCases;
+
+public class test {
+	String a = this.toString();
+	static String b;
+	
+	private void a(String b){
+		System.out.println(this.a);
+	}
+	public void main(String[] args) {
+		a("");
+		System.out.println(b);
+	}
+}
